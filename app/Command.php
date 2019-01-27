b15acb04a45e28f2a1ca23480ee72b63ec4d8f4a
Martin Chobanyan <foxmart36@gmail.com>
2019-01-27 13:22:24 +0400

    #add_desc

--- app/Command.php
+++ app/Command.php
@@ -8,10 +8,10 @@ class Command extends Model
 {
     //
     public function stadium(){
-        return $this->belongsTo('App\Stadium');
+        return $this->belongsTo('App\Stadium', 'id', 'stadium_id');
     }
     public function player(){
-        return $this->hasMany('App\Player', 'command_id', 'id');
+        return $this->hasMany('App\Player', 'command_id', 'id'); // 'his key'(in his table) that will be compared with 'my key'(in my table);
     }
 
     protected $guarded = [];
--- app/Player.php
+++ app/Player.php
@@ -8,7 +8,7 @@ class Player extends Model
 {
     //
     public function command(){
-        return $this->belongsTo('App\Command');
+        return $this->belongsTo('App\Command', 'id', 'command_id'); // 'his key'(in his table), that will be compared with 'my key'(in my table)
     }
 
     protected $guarded = [];
