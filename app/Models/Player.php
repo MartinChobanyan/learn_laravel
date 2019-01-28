3df9580bd7f3bc5217567fd7000bf17c8fd5e487
Martin Chobanyan <foxmart36@gmail.com>
2019-01-28 19:31:52 +0400

    #change

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -10,6 +10,9 @@ class Player extends Model
     public function team(){
         return $this->belongsTo(Team::class, 'team_id', 'id');
     }
+    public function roles(){
+        return $this->belongsToMany(Role::class);
+    }
 
     protected $guarded = [];
     protected $hidden = ['secret'];
