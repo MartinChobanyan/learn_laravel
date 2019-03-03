3a85ea30de2338920790bc42d7da85e139b7866c
Martin Chobanyan <foxmart36@gmail.com>
2019-03-03 16:31:15 +0400

    #edit player-editor modal

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -65,7 +65,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="editorModalLabel">Player Editor</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="outline:none">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
