cc2feae167cec185ff487d1f6b3f84c0da93a9ed
Martin Chobanyan <foxmart36@gmail.com>
2019-03-05 23:00:10 +0400

    #add #db users table row #phone & #update registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,14 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="phone" class="col-md-4 col-form-label text-md-right">Phone Number</label>
+
+                            <div class="col-md-6">
+                                <input id="phone" type="tel" class="form-control" name="phone" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
