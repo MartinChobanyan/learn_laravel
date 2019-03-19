9c66b8ce02022d48ba2f60d4b001a00ced708515
Martin Chobanyan <foxmart36@gmail.com>
2019-03-19 23:16:29 +0400

    #fix seeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -17,9 +17,9 @@ class PostsTableSeeder extends Seeder
         foreach(User::get() as $user){
             Post::create([
                 'author_id' => $user->id,
-                'photo' => bcrypt('photo'),
-                'title' => bcrypt('title'),
-                'content' => bcrypt('content')
+                'photo' => 'https://static1.squarespace.com/static/53adb125e4b094aac18a8ee7/t/5b8ee8a7575d1f5df42a02d0/1536092505155/F1SavannahCat-7.jpg?format=500w',
+                'title' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi',
+                'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi adipisci aliquam quo ex accusamus nesciunt iure maxime sed aspernatur. Perferendis quaerat eum id ex officia laboriosam tempora culpa asperiores omnis?'
             ]);
         }
     }
