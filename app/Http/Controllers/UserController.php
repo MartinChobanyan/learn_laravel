28d5cd785d549a0d06bb3923ffc92722681d0ae9
Martin Chobanyan <foxmart36@gmail.com>
2019-03-19 20:32:59 +0400

    #move #model User into #directory App\Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\User;
+use App\Models\User;
 
 class UserController extends Controller
 {
