34d6392df9783b74be87c060e3ae3d93cb4f074f
Martin Chobanyan <foxmart36@gmail.com>
2019-03-26 20:24:12 +0400

    #add storage/app files linking #artisan #command

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,27 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command(
+    'make:storagelink 
+    {path : path that must be linked from (storage/app/...)} 
+    {link_path : link path (public/...)}', 
+    function($path, $link_path){
+        $path = $this->argument('path');
+        $link_path = $this->argument('link_path');
+
+        if(!($path & $link_path)){
+            return $this->error('Options do not exist.');
+        }
+
+        if (file_exists(public_path($link_path))) {
+            return $this->error('The "' . $link_path . '" directory already exists.');
+        }
+
+        $this->laravel->make('files')->link(
+            storage_path('app/' . $path), public_path($link_path)
+        );
+
+        $this->info('The ' . $path . ' directory has been linked.');
+    }
+)->describe('Linking storage/app directory files');
