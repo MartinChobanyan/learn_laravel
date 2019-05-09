825ecf60c17211c0eefa796623ba7ab25dda8048
MartinChobanyan <foxmart36@gmail.com>
2019-05-10 02:27:43 +0400

    #improve search #func

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,7 +9,10 @@ class SearchController extends Controller
 {
     public function search(Request $request){
         $data = collect(['q' => $request->search_line]);
-        $players = Player::where('name', 'like', '%'.$data['q'].'%')->orderBy('id', 'ASC')->get();
+        $players = Player::select('id', 'name', 'nick')->where(function ($query) use ($data) {
+            $query->where('name', 'LIKE', '%'.$data['q'].'%')
+                  ->orWhere('nick', 'LIKE', '%'.$data['q'].'%');
+        })->orderBy('id', 'ASC')->get();
         $data->put('players', $players);
         return view('search', ['data' => $data]);
     }
